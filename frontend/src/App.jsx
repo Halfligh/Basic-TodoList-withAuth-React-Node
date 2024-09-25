f0bb19866a53efe6d7d587b26a49ed93686121b3
Sebastien Hartmann <coach.habitudes.apprentissages@gmail.com>
2024-09-25 16:39:29 +0200

    improve frontend animations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -36,41 +36,39 @@ function App() {
 
   return (
     <div className="App">
-      <div className="App-content">
-        <div className={!isAuthenticated ? "App-intro" : "App-intro-Signed"}>
-          <img
-            src={logo}
-            className={!isAuthenticated ? "App-logo" : "App-logo-signed"}
-            alt="logo"
-          />
-          <p>
-            {isAuthenticated
-              ? "Bienvenue, vous êtes connecté. Voici votre Todo-list."
-              : "Veuillez vous connecter pour accéder à votre Todo-list."}
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </div>
-        <div className="App-components">
-          <section className="login-section">
-            {isAuthenticated ? (
-              <>
-                <Logout onLogout={handleLogout} />
-                <TodoList />
-              </>
-            ) : (
-              <Login onLogin={handleLogin} /> // Passe handleLogin en prop
-            )}
+      <div className={!isAuthenticated ? "App-intro" : "App-intro-signed"}>
+        <img src={logo} className={!isAuthenticated ? "App-logo" : "App-logo-signed"} alt="logo" />
+        <p>
+          {isAuthenticated
+            ? "Bienvenue, vous êtes connecté."
+            : "Veuillez vous connecter pour accéder à votre Todo-list."}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </div>
+
+      <div className={isAuthenticated ? "App-content-expanded" : "App-content"}>
+        <section className="login-section">
+          {isAuthenticated ? (
+            <div className="profile-logout">
+              <p>Voici votre Todo-list.</p>
+              <Logout onLogout={handleLogout} />
+            </div>
+          ) : (
+            <Login onLogin={handleLogin} /> // Passe handleLogin en prop
+          )}
+        </section>
+        {isAuthenticated ? (
+          <section className="todo-list-section">
+            <TodoList />
           </section>
-          <section className="todo-list-section"></section>
-          <section className="admin-view-section"></section>
-        </div>
+        ) : null}
       </div>
     </div>
   );
